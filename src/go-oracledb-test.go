c84237214512d6e4bc3d653e11b4f0174e8c5ef2
sugimount <sugi.mount@gmail.com>
2019-02-06 23:21:49 +0000

    success!

--- src/go-oracledb-test.go
+++ src/go-oracledb-test.go
@@ -1,5 +1,46 @@
 package main
 
+import (
+    "database/sql"
+    "fmt"
+
+    _ "github.com/mattn/go-oci8"
+)
+
+func getDSN() string {
+    return "admin/nsfan824u789Fjfoijoj3098j@tutorial1_low" // user/name@host:port/sid
+}
+
+func testSelect(db *sql.DB) error {
+    rows, err := db.Query("SELECT EMPNO,EMPNAME,GENDER_F FROM emp")
+    if err != nil {
+        return err
+    }
+
+    defer rows.Close()
+
+    for rows.Next() {
+        var empno string
+        var empname string
+        var gender_f int
+        rows.Scan(&empno, &empname, &gender_f)
+        fmt.Printf("empno:%s empname:%s, gender:%d\n", empno, empname, gender_f)
+    }
+
+    return nil
+}
+
 func main() {
-    println("Hello, 世界")
+    db, err := sql.Open("oci8", getDSN())
+    if err != nil {
+        fmt.Println(err)
+        return
+    }
+
+    defer db.Close()
+
+    if err = testSelect(db); err != nil {
+        fmt.Println(err)
+        return
+    }
 }
